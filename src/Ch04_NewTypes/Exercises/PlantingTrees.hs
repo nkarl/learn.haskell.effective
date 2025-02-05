8d237e3a0bd6a453d31daac7c439e687a170658a
nkarl <21993921+nkarl@users.noreply.github.com>
2025-02-05 13:02:56 -0800

    Studying Chapter 04. Exercise Planting Trees. Added @make@ and @flatten@ functions for convenience.

--- src/Ch04_NewTypes/Exercises/PlantingTrees.hs
+++ src/Ch04_NewTypes/Exercises/PlantingTrees.hs
@@ -3,7 +3,9 @@ module Ch04_NewTypes.Exercises.PlantingTrees where
 import Prelude
 
 {-
-    NOTE: Write the definition of the binary tree type, and then implement the 3 functions below.
+    NOTE:
+    - [x] Write the definition of the binary tree type, and then
+    - [ ] implement the 3 functions below.
 -}
 
 {- | This co-product type represents a binary tree. There are 2 data variants:
@@ -15,15 +17,34 @@ import Prelude
 data BinaryTree a
     = Leaf
     | Branch (BinaryTree a) a (BinaryTree a)
+    deriving (Show)
 
 -- | a trivial alias in order to type less.
 type Tree = BinaryTree
 
+-- | Creates a @Tree@ from a @List@ of elements.
+make :: (Show a, Ord a) => [a] -> Tree a
+make [] = Leaf
+make [a] = Branch Leaf a Leaf
+make (a : b : xs)
+    | b <= a = Branch (make (b : xs)) a Leaf
+    | otherwise = Branch Leaf a (make (b : xs))
+
+-- | Converts a @Tree@ into a @Monoid@.
+flatten :: (Show a) => Tree a -> [a]
+flatten t = go t []
+  where
+    go Leaf acc = acc
+    go (Branch left a right) acc = (a : acc) <> go left [] <> go right []
+
+-- | Shows a @Tree@ of monomorphic @String@ in a human readable form.
 showStringTree :: Tree String -> String
 showStringTree = undefined
 
+-- | Adds an @Int@ element to a @Tree@ of monomorphic @Int@.
 addElementToIntTree :: Tree Int -> Int -> Tree Int
 addElementToIntTree = undefined
 
+-- | Checks if a monomorphic @Tree Int@ contains some @Int@ element.
 doesIntExist :: Tree Int -> Int -> Bool
 doesIntExist = undefined
