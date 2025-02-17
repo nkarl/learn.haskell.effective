cf60a1b02a294341536471dc07ea9950212c0bca
nkarl <21993921+nkarl@users.noreply.github.com>
2025-02-17 12:23:52 -0800

    Studying Chapter 07 IO and Lazyness. Done with Exercise 2. Buidling a command line calculator.

---
+++ lib/Ch07_IO/Exercises/IOCalculator.hs
@@ -0,0 +1,35 @@
+module Ch07_IO.Exercises.IOCalculator where
+
+import Control.Arrow
+import Data.List.Split
+import Prelude
+
+-- | Attempts to map @[String]@ to a @[Int]@.
+mapToNums :: [String] -> [Int]
+mapToNums = (read @Int <$>)
+
+-- | Splits an input string at white-space.
+splitExpr :: String -> [String]
+splitExpr = splitOn " "
+
+-- | Performs a sum in IO. Asks the user for 2 numbers separated by a white-space. Computes their sum.
+runSum :: IO ()
+runSum = do
+    input <- getLine
+    print $ sum $ (mapToNums <<< splitExpr) input
+
+-- | Maps the operator symbol from the user input to the appropriate arithmetic function. Computes the result.
+evalExpr :: [String] -> Int
+evalExpr expr = case expr of
+    "+" : _ -> sum nums
+    "-" : _ -> foldl (-) 0 nums
+    "*" : _ -> product nums
+    _ -> error "not supported"
+  where
+    nums = mapToNums $ tail expr
+
+-- | Runs @evalExpr@ in IO. Asks the user for an prefix expression and evaluates it in IO.
+runCalc :: IO ()
+runCalc = do
+    expr <- splitExpr <$> getLine
+    print $ evalExpr expr
